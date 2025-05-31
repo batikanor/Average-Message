860fec504072faca9cd7c58c43a830ae8ce80d02
BATIKAN BORA ORMANCI <batikanor@gmail.com>
2025-05-31 17:04:08 +0200

    Create empty frontend

---
+++ frontend/next.config.mjs
@@ -0,0 +1,26 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Enable static exports in production
+  output: process.env.NODE_ENV === "production" ? "export" : undefined,
+
+  // Set base path only for GitHub Pages, leave empty if using a custom domain
+  // basePath: process.env.NODE_ENV === 'production' ? '/nextathon-template' : '',
+  basePath: process.env.NODE_ENV === "production" ? "" : "",
+
+  // Disable server-based image optimization for static export compatibility
+  images: {
+    unoptimized: true,
+  },
+
+  // Set assetPrefix for production if assets are hosted separately
+  // assetPrefix:
+  // process.env.NODE_ENV === "production"
+  // ? "https://www.batikanor.com/nextathon-template"
+  // : "",
+  assetPrefix: "",
+
+  // Enable trailing slashes in URLs for correct resolution
+  trailingSlash: true,
+};
+
+export default nextConfig;
