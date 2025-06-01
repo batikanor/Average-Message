b6c2c5f5ee997d3fb13447a48134b061817b72c6
BATIKAN BORA ORMANCI <batikanor@gmail.com>
2025-06-01 07:18:27 +0200

    Minor fix

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,4 +1,4 @@
-import { MinikitProvider } from "@worldcoin/minikit-react";
+import { MiniKitProvider } from "@worldcoin/minikit-js/minikit-provider";
 import localFont from "next/font/local";
 import "./globals.css";
 // import Navbar from '../components/Navbar';
@@ -24,12 +24,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
-      <MinikitProvider>
+      <MiniKitProvider>
         <body className="antialiased min-h-screen w-full bg-gradient-to-br from-gray-900 via-black to-gray-800">
           {/* Minimal layout: just render children centered on a dark background */}
           {children}
         </body>
-      </MinikitProvider>
+      </MiniKitProvider>
     </html>
   );
 }
